34f55081ce408cfbad22ce5567269065b9211bca
Kaushik Kavindra M <mkaushikkavindra@gmail.com>
2026-04-29 17:54:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,39 @@ document.querySelectorAll(".learnmore").forEach(button => {
       : "READ MORE";
   });
 });
+
+
+const posts = document.querySelectorAll("main section article");
+const buttons = document.querySelectorAll(".pagingbtn");
+
+const postsPerPage = 2;
+
+function showPage(page) {
+  const start = (page - 1) * postsPerPage;
+  const end = start + postsPerPage;
+
+  posts.forEach((post, index) => {
+    if (index >= start && index < end) {
+      post.style.display = "grid"; // your layout uses grid
+    } else {
+      post.style.display = "none";
+    }
+  });
+}
+showPage(1);
+
+// button click
+buttons.forEach((btn, index) => {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // remove active from all
+    buttons.forEach(b => b.classList.remove("active"));
+
+    // add active to clicked
+    this.classList.add("active");
+
+    // show page
+    showPage(index + 1);
+  });
+});
